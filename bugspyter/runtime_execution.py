e9873e5da8258aeb0930b4d4d82d57ffe02db848
dabby04 <97193496+dabby04@users.noreply.github.com>
2025-11-20 01:51:40 -0800

    added user_execution_order

--- bugspyter/runtime_execution.py
+++ bugspyter/runtime_execution.py
@@ -2,6 +2,9 @@ from pathlib import Path
 import nbformat
 import ast
 import papermill as pm
+import os
+from nbclient import NotebookClient
+from nbclient.exceptions import CellExecutionError
 
 def load_notebook(notebook_path)-> nbformat.NotebookNode:
     nb = nbformat.read(notebook_path, as_version=4)
@@ -50,7 +53,7 @@ def get_notebook_metadata(nb:nbformat.NotebookNode):
                                })
     return metadata_formatted
 
-def execute_notebook_linear(notebook_path):
+def execute_notebook_linear(notebook_path: str):
     notebook_file_path = Path(notebook_path)
     output = notebook_file_path.stem +"_output.ipynb"
     try:
@@ -62,42 +65,84 @@ def execute_notebook_linear(notebook_path):
     except pm.PapermillExecutionError as e:
         pass
     finally:
-        return output
-
-def return_runtime_cells(nb:nbformat.NotebookNode):
-    runtime_cells = []
-
-    for index, cell in enumerate(nb.cells):
-        if cell.cell_type != "code":
-            continue
-        
-        execution_info = cell.metadata.get("execution")
-        if not execution_info:
-            continue
-        
-        metadata=cell.metadata
-        pm_meta = metadata.get("papermill", {})
-        errors = []
-        
-        if(pm_meta.get("exception") is True):
-            for out in cell.get("outputs", []):
-                if out.get("output_type") == "error":
-                   tb_list = out.get("traceback", [])
-                   errors.append({
-                        "exception_type": out.get("ename"),
-                        "exception_message": out.get("evalue"),
-                        "traceback": "\n".join(tb_list) if tb_list else None
-                    })
+        runtime_cells = []
+        nb = load_notebook(output)
+        for index, cell in enumerate(nb.cells):
+            if cell.cell_type != "code":
+                continue
             
-        runtime_cells.append({
-            "cell_index": index,
-            "source": cell.source,
-            "execution_count": cell.get("execution_count"),
-            "start_time": pm_meta.get("start_time"),
-            "end_time": pm_meta.get("end_time"),
-            "duration": pm_meta.get("duration"),
-            "error": pm_meta.get("exception", False),
-            "errors":errors,
-            "outputs": cell.get("outputs", [])
-        })
-    return runtime_cells
+            execution_info = cell.metadata.get("execution")
+            if not execution_info:
+                continue
+            
+            metadata=cell.metadata
+            pm_meta = metadata.get("papermill", {})
+            errors = []
+            
+            if(pm_meta.get("exception") is True):
+                for out in cell.get("outputs", []):
+                    if out.get("output_type") == "error":
+                        tb_list = out.get("traceback", [])
+                        errors.append({
+                                "exception_type": out.get("ename"),
+                                "exception_message": out.get("evalue"),
+                                "traceback": "\n".join(tb_list) if tb_list else None
+                            })
+                
+            runtime_cells.append({
+                "cell_index": index,
+                "source": cell.source,
+                "execution_count": cell.get("execution_count"),
+                "start_time": pm_meta.get("start_time"),
+                "end_time": pm_meta.get("end_time"),
+                "duration": pm_meta.get("duration"),
+                "error": pm_meta.get("exception", False),
+                "errors":errors,
+                "outputs": cell.get("outputs", [])
+            })
+        
+        if os.path.exists(output):
+            os.remove(output)
+        return runtime_cells
+
+def execute_notebook_user_order(nb:nbformat.NotebookNode, execution_order):
+    """ Execute the notebook following the user's execution order"""
+    
+    kernelspec = nb.metadata.get("kernelspec", {})
+    kernel_name = kernelspec.get("name", None)
+    
+    client = NotebookClient(
+        nb,
+        timeout=600,
+        kernel_name=kernel_name
+    )
+        
+    results = []
+    try:
+        with client.setup_kernel():
+            for item in execution_order:
+                cell_index = item["cell_id"]
+                cell = nb.cells[cell_index]
+
+                try:
+                    client.execute_cell(cell, cell_index)
+                    error = False
+                except CellExecutionError:
+                    error = True
+
+                results.append({
+                    "cell_index": cell_index,
+                    "source": cell.source,
+                    "execution_count": cell.execution_count,
+                    "outputs": cell.outputs,
+                    "error": error
+                })
+
+                if error:
+                    break
+
+    finally:
+        # Ensures cleanup if needed
+        pass
+
+    return results
